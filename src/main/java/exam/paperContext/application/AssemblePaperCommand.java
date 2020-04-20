82c845cd61fd2aa87948a693a960b534692d3df6
xiang3 <ZX814768>
2020-04-20 16:55:20 +0800

    init project

--- src/main/java/exam/paperContext/application/AssemblePaperCommand.java
+++ src/main/java/exam/paperContext/application/AssemblePaperCommand.java
@@ -1,19 +1,20 @@
 package exam.paperContext.application;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Value;
+import lombok.*;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AssemblePaperCommand {
     private String teacherId;
     private List<Quiz> quizzes;
 
-    @Value
+    @Data
+    @Getter
+    @Setter
     public static class Quiz {
         private String quizId;
         private int score;
